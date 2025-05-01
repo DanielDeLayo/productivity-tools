aca8f633a670f5bc0f646312bf856aee84aa88cf
Daniel DeLayo <dandelayo@gmail.com>
2025-05-01 11:36:31 -0400

    no sampling by default

--- cilkiaf/cilkiaf.h
+++ cilkiaf/cilkiaf.h
@@ -21,10 +21,10 @@
 //#define CACHE_LINE_SIZE 4096
 
 // Compiler options. Currnetly they're overrides teehee
-//#define IAF_VERIFY 1
+#define IAF_VERIFY 1
 //#define IAF_GLOBAL 1
 //#define IAF_SAMPLE 1
-#define IAF_LOG 1
+//#define IAF_LOG 1
 
 
 #define CILKTOOL_API extern "C" __attribute__((visibility("default")))
