145bc0d0263d185f594d4ba1cc1a23fb5b2832f6
Daniel DeLayo <dandelayo@gmail.com>
2025-05-09 14:41:25 -0400

    fixing last commit

--- cilkiaf/cilkiaf.cpp
+++ cilkiaf/cilkiaf.cpp
@@ -57,10 +57,10 @@ CilkiafImpl_t::~CilkiafImpl_t() {
     }
 #endif
 #ifdef IAF_SAMPLE_ONE
+    outs_red << "sampled " << i << " " << 0 << std::endl; //partition 0
     local_iafs[i].csv_success_function(outs_red, local_iafs[i].get_success_function(), 1);
 #endif
 #ifdef IAF_VERIFY
-    outs_red << "sampled " << i << " " << 0 << std::endl; //partition 0
     outs_red << "verify " << i << std::endl;
     local_verify_iafs[i].csv_success_function(outs_red, local_verify_iafs[i].get_success_function(), 1);
 #endif
